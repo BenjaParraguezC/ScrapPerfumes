e5f8854cf2aeec2798e5d120a67860f437c4225a
Benjamin Parraguez C. <62433973+BenjaParraguezC@users.noreply.github.com>
2024-09-25 22:03:06 -0300

    Nuevas tiendas

--- scrap.py
+++ scrap.py
@@ -32,6 +32,25 @@ def obtener_info_producto(url, selector_precio, sitio_nombre):
                 match = re.search(r'\$\d+(?:\.\d+)?', texto_precio)
                 if match:
                     return match.group(0)
+        elif sitio_nombre == "HypeFragrances":
+            elemento_precio = sopa.select_one(selector_precio)
+            if elemento_precio:
+                precio = elemento_precio['content']
+                return precio.replace(",", ".") 
+        elif sitio_nombre == "OfertaPerfumes":
+            elemento_precio = sopa.select_one(selector_precio)
+            if elemento_precio:
+                precio = elemento_precio['content']
+                return f"${int(precio):,.0f}".replace(",", ".")
+            else:
+                return "Precio no encontrado"
+        elif sitio_nombre == "Lodoro":
+            elemento_precio = sopa.select_one(selector_precio)
+            if elemento_precio:
+                precio_texto = elemento_precio.text.strip()
+                match = re.search(r'\$[\d.,]+', precio_texto)
+                if match:
+                    return match.group(0)
         else:
             elemento_precio = sopa.select_one(selector_precio)
             if elemento_precio:
@@ -81,12 +100,54 @@ sitios = [
         "url_base": "https://silkperfumes.cl/search?type=product%2C&options%5Bprefix%5D=last&q=",
         "parametro_busqueda": "&filter.p.product_type=",
         "selector_precio": "strong.price_current" 
+    },
+    {
+        "nombre": "HypeFragrances",
+        "url_base": "https://hypefragrances.cl/?s=",
+        "parametro_busqueda": "&post_type=product&dgwt_wcas=1",
+        "selector_precio": "meta[name='twitter:data1']"
+    },
+    {
+        "nombre": "ZonaPerfumes",
+        "url_base": "https://www.zonaperfumes.cl/?s=",
+        "parametro_busqueda": "&post_type=product",
+        "selector_precio": "span.woocommerce-Price-amount bdi"
+    },
+    {
+        "nombre": "OfertaPerfumes",
+        "url_base": "https://www.ofertaperfumes.cl/module/ambjolisearch/jolisearch?s=",
+        "parametro_busqueda": "",
+        "selector_precio": "span.price"
+    },
+    {
+        "nombre": "MultimarcasPerfume",
+        "url_base": "https://multimarcasperfumes.cl/search?type=product&q=",
+        "parametro_busqueda": "*",
+        "selector_precio": "span.product-thumbnail__price.price.sale span.money"
+    },
+    {
+        "nombre": "Yauras",
+        "url_base": "https://yauras.cl/a/search?q=",
+        "parametro_busqueda": "",
+        "selector_precio": "div.price__current.price__current--emphasize.price__current--on-sale span.money"
+    },
+    {
+        "nombre": "LocuraPerfumes",
+        "url_base": "https://locuraperfumes.cl/search?q=",
+        "parametro_busqueda": "&options%5Bprefix%5D=last",
+        "selector_precio": "span.price-item.price-item--regular"
+    },
+    {
+        "nombre": "Lodoro",
+        "url_base": "https://www.lodoro.cl/search?type=product&q=",
+        "parametro_busqueda": "",
+        "selector_precio": "span.price.price--highlight"
     }
 ]
 
 # Nombre y marca del perfume a buscar
-marca_perfume = "MontBlanc"
-nombre_perfume = "Legend Spirit"
+marca_perfume = "Rasasi"
+nombre_perfume = "Hawas"
 
 # Realizar la búsqueda
 resultados = buscar_perfume(marca_perfume, nombre_perfume, sitios)
